d0fdb50e4c9010011f63c613d90096d6e0cc797b
Matthew Norrish <matt.norrish@clavisinsight.com>
2016-12-22 08:29:23 +1300

    add and configure TypeScript

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+  entry: './app.ts',
+
+  output: {
+    path: './dist', // the directory to output bundled files to
+    publicPath: 'dist', // the web path prefix for bundled files
+    filename: 'bundle.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js', '.json'],
+  },
+
+};
